fd0da580c549c76d685c477d0acf07dcfce7ad93
Shamsul Alam <jccbuet1@gmail.com>
2022-08-20 19:57:01 +0600

    Excel module Updated

--- excel/__init__.py
+++ excel/__init__.py
@@ -28,7 +28,7 @@ class Excel:
                            )
         return df
 
-    def vlookup(self, lookup_value_series: pd.Series, lookup_array_top_left_cell: str,
+    def vlookup(self, _reference_series: pd.Series, lookup_array_top_left_cell: str,
                 lookup_array_lower_right_cell: str,
                 x_shift: int) -> pd.Series:
         """
@@ -36,7 +36,7 @@ class Excel:
         """
         lookup_df = self.slice(lookup_array_top_left_cell, lookup_array_lower_right_cell)
         output_series = pd.Series()
-        for value in lookup_value_series:
+        for value in _reference_series:
             matched_row = lookup_df[lookup_df.iloc[:, 0] == value]
             if not matched_row.empty:
                 output_series.loc[len(output_series)] = matched_row.iloc[0, x_shift]
@@ -45,31 +45,24 @@ class Excel:
         return output_series
 
 
-file_path = r'I:\My Drive\IMD\Analysis\Generation\daily report P2 with fuel type.xlsm'
-xl1 = Excel(file_path=file_path, sheet_name='P2')
-# df1 = xl1.slice('C9', 'C200')
-# s1 = df1.iloc[:, 0]  # for converting df to series
-# op = xl1.vlookup(s1, 'C9', 'I150', 6)
-# op.to_excel('lol.xlsx')
-# b = 4
+refernece_file_path = r'J:\My Drive\IMD\Analysis\Generation\Plant Factor Calc\Reference Sheet Daily Report P2.xlsm'
+xl1 = Excel(file_path=refernece_file_path, sheet_name='P2')
+
 
 reference_frame = xl1.slice('C9', 'H174')
 reference_series = xl1.slice('C9', 'C174').iloc[:, 0]
 
-# vlookup_result_series = xl1.vlookup(reference_series, 'C9', 'I150', 6)
-# reference_frame.loc[:, reference_frame.columns[-1] + 1] = vlookup_result_series
-b = 0
-
-folder_directory = r'C:\Users\hE\Downloads\July2022\July2022'
+folder_directory = r'C:\Users\hE\Downloads\August2022\August2022\New folder'
 files = os.listdir(folder_directory)
 excel_files = [file for file in files if file.endswith('.xlsx') or file.endswith('.xlsm')]
 for file in excel_files:
     full_file_path = os.path.join(folder_directory, file)
     xl = Excel(file_path=full_file_path, sheet_name='P2')
-    vlookup_result_series = xl.vlookup(reference_series, 'C9', 'I182', 6)
+    vlookup_result_series = xl.vlookup(reference_series, 'C9', 'J200', 6)  # 6-> kWh, 7 -> Comment
     date = date_parse(file, dayfirst=True, yearfirst=False, fuzzy=True)
     # reference_frame.loc[:, f'{date.year}.{date.month}/{date.day}'] = vlookup_result_series
-    reference_frame.loc[:, f'{date.day - 1} {date.strftime("%b")}'] = vlookup_result_series
+    actual_date = date - pd.to_timedelta('1d')
+    reference_frame.loc[:, f'{actual_date.day} {actual_date.strftime("%b")}'] = vlookup_result_series
 
 reference_frame.to_excel('op.xlsx')
 b = 0
